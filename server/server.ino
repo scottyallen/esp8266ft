20b21289d3dcdbd97ac7c8c8d9b2564bdb882cf9
Scotty Allen <scotty@scottyallen.com>
2016-08-11 10:44:26 -0700

    Tweaks to try and improve speed, and add rainbow

--- server/server.ino
+++ server/server.ino
@@ -15,6 +15,7 @@ FASTLED_USING_NAMESPACE
 const char* ssid     = "Noisebridge Legacy 2.4 gHz";
 
 #define BRIGHTNESS          96
+#define NETWORK_TIMEOUT     10
 
 int count_leds(const int* num_leds_arr, const int len) {
   int count = 0;
@@ -47,12 +48,14 @@ int packetSize = 0;
 static CRGB leds[810];
 
 int last = 0;
-int fps = 20;
+int fps = 30;
 int time_between_frames = 1000 / fps;
 
 int last_packet = 0;
 int max_incoming_fps = fps;
 int min_packet_interval = 1000 / max_incoming_fps;
+
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 
 struct ImageMetaInfo {
   int width;
@@ -74,8 +77,8 @@ void setup() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
-//   WiFi.begin(ssid);
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid);
+  //   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -108,11 +111,7 @@ void setup() {
 
 void loop() {
   if (unhandled) {
-    // If this runs too frequently, the network traffic can back up and overwhelm the network stack, which
-    // doesn't seem to gracefully discard packets.
-    if (millis() - last < time_between_frames) {
-      return;
-    }
+
     int start = millis();
     ImageMetaInfo img_info = {0};
     img_info.width = WIDTH;
@@ -121,29 +120,36 @@ void loop() {
     const char *pixel_pos = ReadImageData(packetBuffer, packetSize,
                                           &img_info);
 
-    for (int y = 0; y < img_info.height; ++y) {
-      for (int x = 0; x < img_info.width; ++x) {
-        const byte red = *pixel_pos++;
-        const byte green  = *pixel_pos++;
-        const byte blue = *pixel_pos++;
+    for (int x = 0; x < img_info.width; ++x) {
+      const byte red = *pixel_pos++;
+      const byte green  = *pixel_pos++;
+      const byte blue = *pixel_pos++;
 
-       leds[x + y * img_info.width] = CRGB(red, green, blue);
-      }
+      leds[x + img_info.offset_x] = CRGB(red, green, blue);
     }
-    FastLED.show();
-    unhandled = 0;
-
+    // If this runs too frequently, the network traffic can back up and overwhelm the network stack, which
+    // doesn't seem to gracefully discard packets.
+    if (millis() - last > time_between_frames) {
+      FastLED.show();
+    }
     last = millis();
+    unhandled = 0;
+  } else if (millis() - last_packet > (1000 * NETWORK_TIMEOUT) ) {
+    if (millis() - last < time_between_frames) {
+      return;
+    }
+    int start = millis();
+    rainbowWithGlitter();
+    FastLED.show();
+  }
+  EVERY_N_MILLISECONDS( 20 ) {
+    gHue++;  // slowly cycle the "base color" through the rainbow
   }
 }
 
 void recv(void *arg,
           udp_pcb *upcb, pbuf *p,
           ip_addr_t *addr, u16_t port) {
-  if (millis() - last_packet < min_packet_interval) {
-    pbuf_free(p);
-    return;
-  }
   packetSize = p->tot_len;
   pbuf_copy_partial(p, packetBuffer, kBufferSize, 0);
   pbuf_free(p);
@@ -152,37 +158,37 @@ void recv(void *arg,
 }
 
 static const char *skipWhitespace(const char *buffer, const char *end) {
- for (;;) {
-   while (buffer < end && isspace(*buffer))
-     ++buffer;
-   if (buffer >= end)
-     return NULL;
-   if (*buffer == '#') {
-     while (buffer < end && *buffer != '\n') // read to end of line.
-       ++buffer;
-     continue;  // Back to whitespace eating.
-   }
-   return buffer;
- }
+  for (;;) {
+    while (buffer < end && isspace(*buffer))
+      ++buffer;
+    if (buffer >= end)
+      return NULL;
+    if (*buffer == '#') {
+      while (buffer < end && *buffer != '\n') // read to end of line.
+        ++buffer;
+      continue;  // Back to whitespace eating.
+    }
+    return buffer;
+  }
 }
 
 // Read next number. Start reading at *start; modifies the *start pointer
 // to point to the character just after the decimal number or NULL if reading
 // was not successful.
 static int readNextNumber(const char **start, const char *end) {
- const char *start_number = skipWhitespace(*start, end);
- if (start_number == NULL) {
-   *start = NULL;
-   return 0;
- }
- char *end_number = NULL;
- int result = strtol(start_number, &end_number, 10);
- if (end_number == start_number) {
-   *start = NULL;
-   return 0;
- }
- *start = end_number;
- return result;
+  const char *start_number = skipWhitespace(*start, end);
+  if (start_number == NULL) {
+    *start = NULL;
+    return 0;
+  }
+  char *end_number = NULL;
+  int result = strtol(start_number, &end_number, 10);
+  if (end_number == start_number) {
+    *start = NULL;
+    return 0;
+  }
+  *start = end_number;
+  return result;
 }
 
 const char *ReadImageData(const char *in_buffer, size_t buf_len,
@@ -222,4 +228,27 @@ const char *ReadImageData(const char *in_buffer, size_t buf_len,
   info->height = height;
   info->range = range;
   return parse_buffer;
+}
+
+void rainbow()
+{
+  // FastLED's built-in rainbow generator
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+}
+
+void addGlitter( fract8 chanceOfGlitter);
+
+void rainbowWithGlitter()
+{
+  //Serial.println("rainbowWithGlitter()");
+  // built-in FastLED rainbow, plus some random sparkly glitter
+  rainbow();
+  addGlitter(80);
+}
+
+void addGlitter( fract8 chanceOfGlitter)
+{
+  if ( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
 }
